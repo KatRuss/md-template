87ab94ef8918e7ae94207850a5ec579c3397a708
Katherine Russell <katrussdev@protonmail.com>
2024-03-27 15:54:53 +0000

    feat: specify commit names

--- push.sh
+++ push.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 if [[ `git status --porcelain` ]]; then
   git add .
-  git commit -m "docs: $(date +"%Y-%m-%d %T") snapshot"
+  if [[ $# -eq 0 ]]; then
+    git commit -m "docs: $(date +"%Y-%m-%d %T") snapshot"
+  else
+    git commit -m "$1"
+  fi
   git push origin
 else
   echo 'There are no changes in this vault. Aborting commit...'
